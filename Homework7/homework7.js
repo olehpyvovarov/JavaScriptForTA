4480deca8ac243f56b65c508caa9a9194ae2cca6
olehpyvovarov <oleg.pyvovarov@gmail.com>
2022-10-25 14:16:43 +0300

    JSforTA Homework8

--- Homework7/homework7.js
+++ Homework7/homework7.js
@@ -1,57 +1,121 @@
 //  task_1 let openWindow = window.open("https://w3.school.com", "w3", "width=300, innerHeight=300, left = 700, top = 200");
-let openWindow = window.open("", "", "width=300, height=300");
-setTimeout(function(){
-    openWindow.resizeTo(500,500);
-    setTimeout(() => {
-        openWindow.moveTo(200,200);
-        setTimeout(() => {
-            openWindow.close();
-        }, 6000);
-    }, 4000);
-},2000)
+// let openWindow = window.open("", "", "width=300, height=300");
+// setTimeout(function(){
+//     openWindow.resizeTo(500,500);
+//     setTimeout(() => {
+//         openWindow.moveTo(200,200);
+//         setTimeout(() => {
+//             openWindow.close();
+//         }, 6000);
+//     }, 4000);
+// },2000)
 
-if (navigator.geolocation) {
-navigator.geolocation.getCurrentPosition((pos) => {
-    console.log("latitude: " + pos.coords.latitude);
-    console.log("longtitude: " + pos.coords.longitude);
-    console.log("altitude: " + pos.coords.altitude);
-    console.log("accuracy: " + pos.coords.accuracy);
-    console.log("spped: " + pos.coords.speed);
-}, (e) => {alert(e);}, {
-    enableHighAccuracy:true, setTimeout: 4000, maximumAge: 0
-});
-} else {
-    alert("Your browser does not support geolocation");
-}
+// if (navigator.geolocation) {
+// navigator.geolocation.getCurrentPosition((pos) => {
+//     console.log("latitude: " + pos.coords.latitude);
+//     console.log("longtitude: " + pos.coords.longitude);
+//     console.log("altitude: " + pos.coords.altitude);
+//     console.log("accuracy: " + pos.coords.accuracy);
+//     console.log("spped: " + pos.coords.speed);
+// }, (e) => {alert(e);}, {
+//     enableHighAccuracy:true, setTimeout: 4000, maximumAge: 0
+// });
+// } else {
+//     alert("Your browser does not support geolocation");
+// }
 
 // task_2
 
-function changeCSS() {
-    document.querySelector("#text").addEventListener("click", (event) => {
-        event.target.style.color = "orange";
-        event.target.style.fontWeight = "20px";
-        event.target.style.fontType = "Comic Sans MS";
-    });
-}
-
+// 1st option: <button onclick="changeCSS()>"
+// function changeCSS() {
+//         let text = document.querySelector("p");
+//         text.style.color = "orange";
+//         text.style.fontSize = "20px";
+//         text.style.fontFamily = "Comic Sans MS";
+// }
+// 2nd option:  <button id="changeCSS"
+// let btn = document.querySelector("#changeCSS");
+// btn.addEventListener("click", function(){
+//     let text = document.querySelector("p");
+//     text.style.color = "orange";
+//     text.style.fontSize = "20px";
+//     text.style.fontFamily = "Comic Sans MS";
+// })
 
 // task_3
 
-document.querySelector("#btn1").addEventListener("click", (event) => {
-    event.target.style.backgroundcolor = "blue";                                // background color
-});
-document.querySelector("#btn2").addEventListener("doubleclick", (event) => {
-    event.target.style.backgroundcolor = "pink";                                // background color
-});
-document.querySelector("#btn2").addEventListener("doubleclick", (event) => {
-    event.target.style.backgroundcolor = "yellow";                                // background color
-});
-//or
-let elem = document.documentHTML();
-function changeBackGroundColor (event, color) {
-    event.target.style.backgroundcolor = color;             // or elem.style.... instead of event.target.style...
+function changeBackgroundcolor (color) {
+    document.body.style.backgroundcolor = color;             // or elem.style.... instead of event.target.style...
 }
-let btn1 = document.querySelector("#btn1").addEventListener("click", changeBackGroundColor("blue"));
-let btn2 = document.querySelector("#btn2").addEventListener("doubleclick", changeBackGroundColor("pink"));
-document.querySelector("#btn").addEventListener("mouseover", changeBackGroundColor("yellow"));
-document.querySelector("#btn").addEventListener("mouseout", changeBackGroundColor("white"));
+// or document.querySelector("#btn").addEventListener("mouseout", changeBackGroundColor("white"));
+
+
+// task_4  upon selecting from a dropdown and clicking on a button the dropdown option is deleted
+
+const myBtn = document.querySelector("#myBtn");
+myBtn.addEventListener("click", deleteDropElem);
+function deleteDropElem () {
+    let x = document.getElementById("names");
+    x.remove(x.selectedIndex);
+}
+
+
+// task_5 varios actions on a button
+
+let butt = document.getElementById("liveButton");
+//butt.addEventListener("mouseover", mouseOverFunc);
+butt.addEventListener("click", clickFunc);
+//butt.addEventListener("mouseout", mouseOutFunc);
+
+// function mouseOverFunc(){
+//     document.getElementById("myID").innerHTML += "Mouse on me!<br>";
+// }
+// function mouseOutFunc(){
+//     document.getElementById("myID").innerHTML += "Mouse is not on me!<br>";
+// }
+function clickFunc(){
+    document.getElementById("myID").innerHTML += "I was pressed!<br>";
+}
+
+
+// task_6   following up/tracking Windows size when changed
+
+function displayWindowSize() {
+    let w = window.innerWidth;
+    let h = window.innerHeight;
+    let size = "Width " + w + ", " + "Height " + h;
+    document.getElementById("result").innerHTML = "Window size: " + size;
+}
+window.addEventListener("resize", displayWindowSize);
+
+
+// task_7       realize 2 dropdowns: 'countries' and corresponding 'cities': when a country selected cities dropdown appears and when a city is selected
+//              the country name and the city name is displayed below
+
+const cityArr = {
+    "ger" : ["Steinau", "Bad Soden", "Schluchtern", "Fulda"],
+    "usa" : ["Charlotte", "Salem", "High Point", "Wilmington"],
+    "ukr" : ["Kyiv", "Lviv", "Rivne", "Lutsk"]
+}
+let selec = document.getElementById("country");
+selec.addEventListener("change", createList);
+selec.addEventListener("change", toParagraph);
+
+let citySelec = document.getElementById("cities");
+citySelec.addEventListener("change", toParagraph);
+
+function createList() {
+    citySelec.innerHTML = "";
+    let selectedCountry = selec.value;
+    for (let i = 0; i < cityArr[selectedCountry].length; i +=1) {
+        let newOption = document.createElement("option");
+        newOption.innerHTML = cityArr[selectedCountry][i];
+        citySelec.appendChild(newOption);
+    }
+}
+let parag = document.querySelector("p");
+function toParagraph() {
+    parag.innerHTML = "";
+    parag.innerHTML = selec.options[selec.selectedIndex].text + ", "
+    + citySelec.options[citySelec.selectedIndex].text;
+}
--- Homework8/homework8.js
+++ Homework8/homework8.js
@@ -26,15 +26,16 @@
 // regExp(str12);
 
 
+
 // task_2
 
-// function checkEmail (str2) {
-//     return /^\D\w+@\w+\.\D{2,4}$/i.test(str2);         // needs $ to set up the range of the ending {2,4}
-// }
-// console.log(checkEmail("Op@ano12ther.coaa"));
+function checkEmail (str2) {
+    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/i.test(str2);         // needs $ to set up the range of the ending {2,3}
+}
+console.log(checkEmail("op.op@ano12ther.coa"));
 
 
-// task_3
+// task_3    regex for finding 'd' then 1 or more 'b' then one 'd'
 
 let str3 = "cdbBdbsbz";
 let re3 = /d[bB]+d/g;
@@ -43,12 +44,17 @@ let match3 = str3.match(re3);
 console.log(match3);
 
 
-// task_4
+// task_4   regex to swap words in a string
 
-/\w+\s\w+/
+let str4 = "Java Script";
+let re4 = /(\b\w+\b)\s(\b\w+\b)/;
+let res4 = re4.exec(str4);
+//let res4 = str4.match(re4);
+let newStr4 = res4[2] + res4[1];
+console.log(newStr4);
 
 
-// task_5   bank card validation function  ++
+// task_5   bank card validation function  
 
 function cardValid (str5) {
     return /^\d{4}-?\d{4}-?\d{4}-?\d{4}$/.test(str5);            
@@ -59,7 +65,7 @@ console.log("Card is valid: " + cardValid("4242-4444-4444-4545"));
 // task_6
 
 function checkEmail6 (str6) {
-    if (/\w+-?\w+@\w+\.\D{2,4}$/.test(str6)) {                  // /^[_-]\w+@\w+\.\D{2,4}$/
+    if ( /^\w+([\.-_]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/i.test(str6) ) {                  // /^[_-]\w+@\w+\.\D{2,4}$/
         console.log("You e-mail address is correct");
     } else{ 
         console.log("You e-mail address is NOT correct");
@@ -76,10 +82,11 @@ checkEmail6("3: " + mail3);
 // task_7
 
 function checkLogin (str7) {
-    let re7 = /^\D[A-Za-z0-9[^\W^\.]]{2,10}/g;
+    let re7 = /^([a-z0-9]|[-._](?![-._])){2,10}$/g;  
     //console.log(str7.match(re7));
     console.log(re7.test(str7));
     
 }
 checkLogin("ee1.1ret3");
 checkLogin("ee1*1ret3");
+checkLogin("1ee1.1ret3");
